f1403e10462828c01ce6477fc7c1d5f8cc06a67f
meow <musindelus@gmail.com>
2021-01-16 20:57:38 +0300

    init

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app = express();
+const APP_PORT = process.env.PORT || 5000;
+
+app.use("*",(req, res) =>{
+    res.send("<h1>Welcome to your simple server! Awesome right</h1>");
+});
+
+app.listen(APP_PORT,() => console.log(`hosting @${APP_PORT}`));
