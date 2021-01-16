b1b67df5b68cd457c7d66453e91adeddd609eefc
meow <musindelus@gmail.com>
2021-01-16 22:20:56 +0300

    init2

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,25 @@
+import TBot from 'node-telegram-bot-api';
 import express from 'express';
+
+const token = process.env.TOKEN;
+
+if(!token) {
+    throw new Error("Token not found")
+}
+
+const bot = new TBot(token, { polling: true });
+
+let message = {};
+
+bot.on('message', msg => {
+   message = msg;
+});
 
 const app = express();
 const APP_PORT = process.env.PORT || 5000;
 
 app.use("*",(req, res) =>{
-    res.send("<h1>Welcome to your simple server! Awesome right</h1>");
+    res.send(JSON.stringify(message));
 });
 
-app.listen(APP_PORT,() => console.log(`hosting 11@${APP_PORT}`));
+app.listen(APP_PORT,() => console.log(`hosting @${APP_PORT}`));
