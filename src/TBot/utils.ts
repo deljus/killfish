42558e8d7203a29682c3b22bb8ecce9bffca7fa9
meow <musindelus@gmail.com>
2021-01-18 16:29:55 +0300

    init12

--- src/TBot/utils.ts
+++ src/TBot/utils.ts
@@ -39,5 +39,5 @@ export const INLINE_KEYBOARD = {
 
 export const prepareTask = (tasks: string[], userName: string): string => {
     const task = tasks.map((item, key) => `${key + 1}. ${item}`).join('\n');
-    return `@${userName}! Твое задание на сегодня: \n ${task}`
+    return `@${userName}! Твое задание на сегодня: \n${task}`
 };
