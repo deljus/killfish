dc53b718b537a37422519ff0ec470f28c8ea80e3
meow <musindelus@gmail.com>
2021-01-20 19:04:15 +0300

    init12

--- src/server.ts
+++ src/server.ts
@@ -10,9 +10,5 @@ new Index(process.env.TOKEN);
 
 const app = express();
 
-app.use("*",(_, res) =>{
-    res.send(JSON.stringify([]));
-});
-
 app.use('/static', express.static(__dirname + '/static'));
 app.listen(process.env.PORT || 5000);
